d4109a4e01dfa08c09f6b626f3de9f948bf1ccb9
guoxiaoan <guoxiaioan@qq.com>
2024-09-22 22:06:21 +0800

    NA

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,30 @@
 console.log('service-worker.js loaded');
-self.addEventListener('install', async event => {
-    console.log('Service Worker 安装中...');
-    const cache = await caches.open('pwa-demo-cache');
-    const urlsToCache = [
+// self.addEventListener('install', async event => {
+//     console.log('Service Worker 安装中...');
+//     const cache = await caches.open('pwa-demo-cache');
+//     const urlsToCache = [
+//         '/',
+//         '/styles/main.css',
+//         '/scripts/main.js',
+//         '/fav.png'
+//     ];
+  
+//     event.waitUntil(
+//         cache.addAll(urlsToCache)
+//             .then(() => {
+//                 console.log('Service Worker 所有资源已缓存');
+//             })
+//     );
+// });
+
+self.addEventListener('install', function(event) {  
+  event.waitUntil(  
+    caches.open('pwa-demo-cache').then(function(cache) {  
+      return cache.addAll([  
         '/',
         '/styles/main.css',
-        '/scripts/main.js',
-        '/fav.png'
-    ];
-  
-    event.waitUntil(
-        cache.addAll(urlsToCache)
-            .then(() => {
-                console.log('Service Worker 所有资源已缓存');
-            })
-    );
+        '/scripts/main.js'
+      ]);  
+    })  
+  );  
 });
