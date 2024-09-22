bbc8ab6c8635538c8021f9dc875331168d863d4c
guoxiaoan <guoxiaioan@qq.com>
2024-09-22 17:21:14 +0800

    NA

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,17 @@
-self.addEventListener('install', async event => {  
-    const cache = await caches.open('pwa-demo-cache');  
-    const urlsToCache = [  
-        '/',  
-        '/styles/main.css',  
-        '/scripts/main.js',  
+self.addEventListener('install', async event => {
+    console.log('Service Worker 安装中...');
+    const cache = await caches.open('pwa-demo-cache');
+    const urlsToCache = [
+        '/',
+        '/styles/main.css',
+        '/scripts/main.js',
         '/fav.png'
-    ];  
+    ];
   
-    event.waitUntil(  
-        cache.addAll(urlsToCache)  
-            .then(() => {  
-                console.log('所有资源已缓存');  
-            })  
-    );  
+    event.waitUntil(
+        cache.addAll(urlsToCache)
+            .then(() => {
+                console.log('Service Worker 所有资源已缓存');
+            })
+    );
 });
